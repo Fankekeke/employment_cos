37296bdc671707e12f4c1bf76070612155460acc
Fankekeke <1258846421@qq.com>
2024-09-30 23:26:15 +0800

    fix - 管理员会场编辑

--- frontend/src/views/manage/venue/Venue.vue
+++ frontend/src/views/manage/venue/Venue.vue
@@ -83,7 +83,7 @@
           </template>
         </template>
         <template slot="operation" slot-scope="text, record">
-          <a-icon type="setting" theme="twoTone" twoToneColor="#4a9ff5" @click="edit(record)" title="修 改"></a-icon>
+<!--          <a-icon type="setting" theme="twoTone" twoToneColor="#4a9ff5" @click="edit(record)" title="修 改"></a-icon>-->
           <a-icon type="file-search" @click="venueViewOpen(record)" title="详 情" style="margin-left: 15px"></a-icon>
         </template>
       </a-table>
